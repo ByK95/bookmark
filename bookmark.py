376441edbb0ec64e05ed833423f8e08d5cc9cefc
ByK95 <ByK95@github.com>
2019-11-28 10:31:04 +0300

    add template generation

--- bookmark.py
+++ bookmark.py
@@ -1,6 +1,7 @@
 import time
 import json
 import os
+from jinja2 import Template
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
 
@@ -18,8 +19,12 @@ def open_pdf_on(driver,path,page=0):
 
 if __name__ == "__main__":
     driver = webdriver.Firefox(executable_path="./geckodriver.exe", options=None)
-    with open("./cache.json") as f:
+    with open("./cache.json",'r') as f:
         cache = json.load(f)
+    with open("./index.jinja2",'r') as f:
+        template = Template(f.read())
+    with open("./index.html",'w') as f:
+        f.write(template.render(books=cache))
     path = os.path.realpath('./index.html')
     driver.get("file:///"+path.replace('\\','/'))
     # open_pdf_on(driver,cache[0]['path'],cache[0]['page'])
