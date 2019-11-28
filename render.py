40620f181b35850460816cccdc8a6761f512219f
ByK95 <ByK95@github.com>
2019-11-29 00:28:32 +0300

    add filename cleaning in render

--- render.py
+++ render.py
@@ -2,10 +2,14 @@ from jinja2 import Template
 import os
 import json
 cache = None
+
+def clean_book_name(path):
+    return os.path.split(path)[-1]
+
 if os.path.isfile('./cache.json'):
         with open("./cache.json",'r') as f:
             cache = json.load(f)
 with open("./index-jinja2.html",'r') as f:
     template = Template(f.read())
 with open("./index.html",'w') as f:
-    f.write(template.render(books=cache))
+    f.write(template.render({"books":cache,"clean":clean_book_name}))
