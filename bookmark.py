dc884ae91417a01cc2fc8e6d3e642f58bcb8b6f0
Typic <life4ever35@gmail.com>
2019-11-28 13:35:48 +0300

    rename template

--- bookmark.py
+++ bookmark.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     if os.path.isfile('./cache.json'):
         with open("./cache.json",'r') as f:
             cache = json.load(f)
-    with open("./index.jinja2",'r') as f:
+    with open("./index-jinja2.html",'r') as f:
         template = Template(f.read())
     with open("./index.html",'w') as f:
         f.write(template.render(books=cache))
