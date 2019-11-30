6d22662f5b69afa184a7d746382701fe92934243
Typic <life4ever35@gmail.com>
2019-11-30 11:07:38 +0300

    add item adding to dom

--- js/main.js
+++ js/main.js
@@ -6,3 +6,11 @@ document.getElementById('addbooks').addEventListener('click',function(){
 function unlock(){
     addbookslock = false;
 }
+
+function addElement(){
+    var para = document.createElement("button");
+    var node = document.createTextNode("This is new.");
+    para.appendChild(node);
+    var element = document.getElementById("toolbarViewerRight");
+    element.appendChild(para);
+}
