be943aa8d0bbc54fd74de35e575fea5f479c80c7
ByK95 <ByK95@github.com>
2019-12-04 22:01:56 +0300

    lintpage

--- bookmark.py
+++ bookmark.py
@@ -1,13 +1,14 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 import time
 import json
 import os
 from jinja2 import Template
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import TimeoutException, NoSuchElementException ,WebDriverException
+from selenium.common.exceptions import TimeoutException, NoSuchElementException, WebDriverException
 import subprocess
 import pathlib
+
 
 def safe_find_element_by_class(driver, elem_class):
     try:
@@ -15,24 +16,29 @@ def safe_find_element_by_class(driver, elem_class):
     except NoSuchElementException:
         return None
 
-def open_pdf_on(driver,page=0):
+
+def open_pdf_on(driver, page=0):
     while safe_find_element_by_class(driver, 'page') is None:
         time.sleep(0.5)
     driver.find_element_by_id('pageNumber').click()
     driver.find_element_by_id('pageNumber').send_keys(page + Keys.ENTER)
 
+
 def write2JSON(obj):
-    with open("./cache.json",'w') as f:
+    with open("./cache.json", 'w') as f:
         f.write(json.dumps(obj))
 
 
 def getJson():
-    if not os.path.isfile("./cache.json"): write2JSON([])
-    with open("./cache.json",'r') as f:
+    if not os.path.isfile("./cache.json"):
+        write2JSON([])
+    with open("./cache.json", 'r') as f:
         return json.load(f)
+
 
 def render_html_page():
     subprocess.call(["python", "./render.py"])
+
 
 def add_books():
     import tkinter as tk
@@ -47,14 +53,17 @@ def add_books():
     files = list(file_path)
     cache = getJson()
     for book in files:
-        cache.append({"path":pathlib.Path(book).as_uri(),"name":pathlib.PurePath(book).name,"page":0})
+        cache.append({"path": pathlib.Path(book).as_uri(),
+                      "name": pathlib.PurePath(book).name, "page": 0})
     write2JSON(cache)
     render_html_page()
     driver.refresh()
     return True
 
+
 def clean_book_name(path):
     return os.path.split(path)[-1]
+
 
 def bind(driver):
     bindjs = """
@@ -67,14 +76,17 @@ def bind(driver):
         }"""
     print(driver.execute_script(bindjs))
 
+
 if __name__ == "__main__":
     lock_page_shifting = False
     index_dict = {}
-    driver = webdriver.Firefox(executable_path="./geckodriver.exe", options=None)
+    driver = webdriver.Firefox(
+        executable_path="./geckodriver.exe", options=None)
     cache = None
-    if not os.path.isfile("./index.html"): render_html_page()
+    if not os.path.isfile("./index.html"):
+        render_html_page()
     path = os.path.realpath('./index.html')
-    index_url = "file:///"+path.replace('\\','/')
+    index_url = "file:///"+path.replace('\\', '/')
     driver.get(index_url)
     try:
         while True:
@@ -85,15 +97,16 @@ if __name__ == "__main__":
                 lock_page_shifting = False
             else:
                 if not lock_page_shifting:
-                    url , page = driver.current_url.split("?page=")
+                    url, page = driver.current_url.split("?page=")
                     if not url in index_dict:
-                        open_pdf_on(driver,page)
+                        open_pdf_on(driver, page)
                         lock_page_shifting = True
                 else:
-                    index_dict[driver.current_url.split("?page=")[0]] = driver.find_element_by_id("pageNumber").get_attribute("value")
+                    index_dict[driver.current_url.split("?page=")[0]] = driver.find_element_by_id(
+                        "pageNumber").get_attribute("value")
             time.sleep(1)
     except WebDriverException:
-        #Render html page on shutdown
+        # Render html page on shutdown
         if len(index_dict) > 0:
             save = getJson()
             for book in save:
