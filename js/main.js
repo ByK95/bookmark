34ff5640f1700d63ebb5cc87914cde03b13cc576
Typic <life4ever35@gmail.com>
2019-12-09 23:43:51 +0300

    add config variable to main.js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 var addbookslock = false;
-document.getElementById('addbooks').addEventListener('click',function(){
-    addbookslock = true;
-})
+var config = false;
 
-function unlock(){
-    addbookslock = false;
+document.getElementById("addbooks").addEventListener("click", function() {
+  addbookslock = true;
+});
+
+function unlock() {
+  addbookslock = false;
+  config = false;
 }
 
-function addElement(){
-    var para = document.createElement("button");
-    var node = document.createTextNode("This is new.");
-    para.appendChild(node);
-    var element = document.getElementById("toolbarViewerRight");
-    element.appendChild(para);
+function addElement() {
+  var para = document.createElement("button");
+  var node = document.createTextNode("This is new.");
+  para.appendChild(node);
+  var element = document.getElementById("toolbarViewerRight");
+  element.appendChild(para);
 }
