3fda972fa6070c42b82550911f7305d732903794
Typic <life4ever35@gmail.com>
2019-12-10 10:00:58 +0300

    add config rendering  into render

--- render.py
+++ render.py
@@ -2,14 +2,20 @@ from jinja2 import Template
 import os
 import json
 cache = None
+configs = None
+
 
 def clean_book_name(path):
     return os.path.split(path)[-1]
 
+
 if os.path.isfile('./cache.json'):
-        with open("./cache.json",'r') as f:
-            cache = json.load(f)
-with open("./index-jinja2.html",'r',encoding='utf-8') as f:
+    with open("./cache.json", 'r') as f:
+        cache = json.load(f)
+    with open("./book_conf.json", 'r') as f:
+        configs = json.load(f)
+with open("./index-jinja2.html", 'r', encoding='utf-8') as f:
     template = Template(f.read())
-with open("./index.html",'w',encoding='utf-8') as f:
-    f.write(template.render({"books":cache,"clean":clean_book_name}))
+with open("./index.html", 'w', encoding='utf-8') as f:
+    f.write(template.render(
+        {"books": cache, "clean": clean_book_name, "configs": configs}))
