d6670dbb877c7dc079a4088468cbec8d00822621
Typic <life4ever35@gmail.com>
2019-12-10 10:04:43 +0300

    add remElement func

--- js/main.js
+++ js/main.js
@@ -20,10 +20,20 @@ function addElement(type, classes, payload, target) {
 }
 
 var a = document.getElementsByClassName("b-config");
+function remElement() {
+    for (let index = 0; index < a.length; index++) {
+        const element = a[index];
+        if (element.children.length == 1) {
+            element.children[0].remove();
+        }
+    }
+}
+
 for (let index = 0; index < a.length; index++) {
     const element = a[index];
     element.addEventListener("click", function(el) {
         config = el.target.innerText;
+        remElement();
         if (el.target.children.length == 0) {
             addElement("i", ["fa", "fa-check"], "", el.target);
         }
