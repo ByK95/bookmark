aab3624983dba503f1bdbbf08022fe50e25e7154
Typic <life4ever35@gmail.com>
2019-12-10 10:00:06 +0300

    change add elem function

--- js/main.js
+++ js/main.js
@@ -10,10 +10,22 @@ function unlock() {
   config = false;
 }
 
-function addElement() {
-  var para = document.createElement("button");
-  var node = document.createTextNode("This is new.");
-  para.appendChild(node);
-  var element = document.getElementById("toolbarViewerRight");
-  element.appendChild(para);
+function addElement(type, classes, payload, target) {
+    // <i class="fa fa-check"></i>
+    var para = document.createElement(type);
+    var node = document.createTextNode(payload);
+    para.classList.add(...classes);
+    para.appendChild(node);
+    target.appendChild(para);
+}
+
+var a = document.getElementsByClassName("b-config");
+for (let index = 0; index < a.length; index++) {
+    const element = a[index];
+    element.addEventListener("click", function(el) {
+        config = el.target.innerText;
+        if (el.target.children.length == 0) {
+            addElement("i", ["fa", "fa-check"], "", el.target);
+        }
+    });
 }
