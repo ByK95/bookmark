e8c03b674385eb68e527c0477515340c57955d36
Typic <byrmkaya97@gmail.com>
2020-03-22 18:50:40 +0300

    convert rendering into database

--- interfaces.py
+++ interfaces.py
@@ -2,21 +2,41 @@ import os
 import json
 from json import JSONEncoder
 
+
 class Book(object):
     def __init__(self, **kwargs):
         for arg in kwargs.items():
-            setattr(self,arg[0],arg[1])
+            setattr(self, arg[0], arg[1])
+
 
 class BookEncoder(JSONEncoder):
     def default(self, o):
         return o.__dict__
+
 
 class BookLoaderInterface(object):
 
     def load_data(self):
         pass
 
-    def save_data(self,data):
+    def save_data(self, data):
+        pass
+
+
+class DbBookLoader(BookLoaderInterface):
+
+    def load_data(self):
+        from db import db
+        datab = db()
+        db = datab.connect()
+        res = db.execute(
+            'SELECT name,path,page FROM current INNER JOIN books ON books.id = current.book_id INNER JOIN history ON history.id = current.history_id;').fetchall()
+        books = []
+        for book in res:
+            books.append(Book(name=book[0], path=book[1], page=book[2]))
+        return books
+
+    def save_data(self, data):
         pass
 
 
@@ -24,16 +44,16 @@ class JsonLoaderInterface(BookLoaderInterface):
 
     def load_data(self):
         self.data = []
-        if not hasattr(self,"path"):
+        if not hasattr(self, "path"):
             raise ValueError()
         if os.path.isfile(self.path):
             with open(self.path, 'r') as f:
                 cache = json.load(f)
                 for item in cache:
                     self.data.append(Book(**item))
-        
-    def save_data(self,datas):
-        if not hasattr(self,"path"):
+
+    def save_data(self, datas):
+        if not hasattr(self, "path"):
             raise ValueError()
         with open(self.path, 'w') as f:
-            f.write(json.dumps(self.data,cls=BookEncoder))
+            f.write(json.dumps(self.data, cls=BookEncoder))
--- render.py
+++ render.py
@@ -1,7 +1,7 @@
 from jinja2 import Template
 import os
 import json
-from interfaces import Book,JsonLoaderInterface
+from interfaces import Book, JsonLoaderInterface, DbBookLoader
 cache = None
 configs = None
 
@@ -10,14 +10,13 @@ def clean_book_name(path):
     return os.path.split(path)[-1]
 
 
-if os.path.isfile('./cache.json'):
-    loader = JsonLoaderInterface()
-    loader.path = "./cache.json"
-    loader.load_data()
+if os.path.isfile('./knowledge.db'):
+    loader = DbBookLoader()
+    books = loader.load_data()
     with open("./book_conf.json", 'r') as f:
         configs = json.load(f)
 with open("./index-jinja2.html", 'r', encoding='utf-8') as f:
     template = Template(f.read())
 with open("./index.html", 'w', encoding='utf-8') as f:
     f.write(template.render(
-        {"books": loader.data, "clean": clean_book_name, "configs": configs}))
+        {"books": books, "clean": clean_book_name, "configs": configs}))
