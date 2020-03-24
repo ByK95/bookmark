a38424b0a2e1d1848a1e40bc844b347bbab021e1
Typic <byrmkaya97@gmail.com>
2020-03-24 17:26:20 +0300

    fix config does not exist

--- render.py
+++ render.py
@@ -13,10 +13,14 @@ def clean_book_name(path):
 if os.path.isfile('./knowledge.db'):
     loader = DbBookLoader()
     loader.load_data()
-    with open("./book_conf.json", 'r') as f:
-        configs = json.load(f)
-with open("./index-jinja2.html", 'r', encoding='utf-8') as f:
-    template = Template(f.read())
+    try:
+        with open("./book_conf.json", 'r') as f:
+            configs = json.load(f)
+    except FileNotFoundError:
+        pass
+    with open("./index-jinja2.html", 'r', encoding='utf-8') as f:
+        template = Template(f.read())
+
 with open("./index.html", 'w', encoding='utf-8') as f:
     f.write(template.render(
         {"books": loader.data, "clean": clean_book_name, "configs": configs}))
