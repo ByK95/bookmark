7e0c66caa0b60e543662cad4589ba6ca97015ed2
Typic <byrmkaya97@gmail.com>
2020-06-12 00:34:21 +0300

    create preferences table on first run

--- db.py
+++ db.py
@@ -9,6 +9,7 @@ db_init = {
         "CREATE TABLE IF NOT EXISTS history (id INTEGER PRIMARY KEY ASC,book_id INTEGER,page TEXT,'time' TIMESTAMP DEFAULT CURRENT_TIMESTAMP);",
     "create_relation":
         "CREATE TABLE IF NOT EXISTS current (book_id INTEGER NOT NULL , history_id);",
+    "create_prefs":"CREATE TABLE IF NOT EXISTS preferences (id INTEGER PRIMARY KEY ASC, name TEXT, style TEXT,zoom INTEGER);",
     "create_history_trigger":
         """CREATE TRIGGER on_read
             AFTER INSERT ON history
@@ -43,6 +44,7 @@ class db(object):
             db.execute(db_init["create_books"])
             db.execute(db_init["create_history"])
             db.execute(db_init["create_relation"])
+            db.execute(db_init["create_prefs"])
             db.execute(db_init["create_onbookadd_trigger"])
             db.execute(db_init["create_history_trigger"])
             db.commit()
