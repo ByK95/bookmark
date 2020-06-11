c43244e002fe0c564049b63300d54d400a18eafa
Typic <byrmkaya97@gmail.com>
2020-06-11 18:16:58 +0300

    add database initializer

--- db.py
+++ db.py
@@ -1,6 +1,23 @@
 import sqlite3
+import os.path
 # c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
 
+db_init = {
+    "create_books":
+        "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY ASC,name TEXT,path TEXT,'time' TIMESTAMP DEFAULT CURRENT_TIMESTAMP);",
+    "create_history":
+        "CREATE TABLE IF NOT EXISTS history (id INTEGER PRIMARY KEY ASC,book_id INTEGER,page TEXT,'time' TIMESTAMP DEFAULT CURRENT_TIMESTAMP);",
+    "create_relation":
+        "CREATE TABLE IF NOT EXISTS current (book_id INTEGER NOT NULL , history_id);",
+    "create_history_trigger":
+        """CREATE TRIGGER on_read
+            AFTER INSERT ON history
+            BEGIN
+                UPDATE current SET history_id = new.id WHERE book_id = new.book_id;
+            END;""",
+    "create_onbookadd_trigger":
+        """CREATE TRIGGER on_book_add AFTER INSERT ON books BEGIN INSERT INTO current(book_id) VALUES(new.id); END;"""
+    }
 
 class Database(object):
     def __init__(self, query):
@@ -21,6 +38,16 @@ class Database(object):
 class db(object):
     def __init__(self):
         self.db_name = "knowledge.db"
+        if not os.path.exists('knowledge.db'):
+            db = sqlite3.connect(self.db_name)
+            db.execute(db_init["create_books"])
+            db.execute(db_init["create_history"])
+            db.execute(db_init["create_relation"])
+            db.execute(db_init["create_onbookadd_trigger"])
+            db.execute(db_init["create_history_trigger"])
+            db.commit()
+            db.close()
+        
 
     def connect(self):
         return sqlite3.connect(self.db_name)
