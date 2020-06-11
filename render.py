54103e7b8abcc9366c6a023daf2462b7ca6272d9
Typic <byrmkaya97@gmail.com>
2020-06-11 23:17:31 +0300

    remove cache checking
    On first run cannot generate template cuz of crash

--- render.py
+++ render.py
@@ -9,18 +9,19 @@ configs = None
 def clean_book_name(path):
     return os.path.split(path)[-1]
 
+def load_if_exists(filename):
+    configs = []
+    if os.path.exists(filename):
+        with open(filename, 'r') as f:
+            configs = json.load(f)
+    return configs
 
-if os.path.isfile('./knowledge.db'):
+if __name__ == "__main__":
     loader = DbBookLoader()
     loader.load_data()
-    try:
-        with open("./book_conf.json", 'r') as f:
-            configs = json.load(f)
-    except FileNotFoundError:
-        pass
+    configs = load_if_exists("./book_conf.json")
     with open("./index-jinja2.html", 'r', encoding='utf-8') as f:
         template = Template(f.read())
-
-with open("./index.html", 'w', encoding='utf-8') as f:
-    f.write(template.render(
-        {"books": loader.data, "clean": clean_book_name, "configs": configs}))
+    with open("./index.html", 'w', encoding='utf-8') as f:
+        f.write(template.render(
+            {"books": loader.data, "clean": clean_book_name, "configs": configs}))
